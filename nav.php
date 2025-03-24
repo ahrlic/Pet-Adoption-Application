345a2854945f6e192c2dbea58acd31716311646b
Ariana Hrlic <ariana.hrlic@gmail.com>
2025-03-24 15:04:39 -0400

    Added Link to signup.html in nav.php

--- nav.php
+++ nav.php
@@ -3,7 +3,7 @@
             <a href="contact.html">Contact</a>
             <a href="petCare.php">Tips</a>
             <a href="page">Insurance</a>
-            <a href="page">page</a>
+            <a href="signup.html">Sign Up</a>
             <a href="page">page</a>
     
         </nav>
