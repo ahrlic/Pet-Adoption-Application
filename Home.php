70ecc936ca1a88a321ac4b868663174116bce751
HJL <167211523+Fiona1217@users.noreply.github.com>
2025-04-19 08:50:04 -0400

    Update Home.php

--- Home.php
+++ Home.php
@@ -1,7 +1,10 @@
-<!--Name: Fiona Ang, Smriti Kohli, Ariana Hrlic-->
-<!--File: home.php-->
-<!--Date: March 26, 2025-->
-<!--Description: This is a html file for Assignment 2 - home.php -->
+<!-- Name: Ariana Hrlic, Fiona Ang, Smriti Kohli
+Author: Ariana Hrlic
+Section: CST 8285 300_301
+File: Home.php
+Date: April 8, 2025
+Description: This is a php file for Assignment2 -->
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,30 +16,20 @@
     <title>Adoptly</title>
     <link rel="stylesheet" href="CSS/homeStyle.css">
 </head>
-
-
 <body>
-
     <header> 
-        <a href="#" class = "logo">Adoptly<span>.</span></a>
-    
         <!--page is a placeholder -->
-        <?php include 'nav.php';?>
-    
+        <?php include 'UpperNavInsurance.php'; ?>
     </header>
-   
 
     <section class="home">
         <h1>Adoptly</h1>
         <br>
         <div class="info">
-        <span> <p>The home for all furry friends, spreading love and helping you find your new best friend. </p></span>
-        <h3>Welcome to our pet adoption services!</h3>
-      
+            <span> <p>The home for all furry friends, spreading love and helping you find your new best friend. </p></span>
+            <h3>Welcome to our pet adoption services!</h3>
         </div>
-
     </section>
-  
 </body>
 <?php include 'footerInsurance.php';?>
 
